92eb3238d74baaa4277a7893e0c2779dfb1981d1
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-26 16:10:26 -0500

    reading the csv and imported packages to effectively read and manipulate data

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#import libraries for project:
+import numpy as np
+import matplotlib.pyplot as plot
+import pandas as pd
 
+dataset = pd.read_csv('~/Desktop/PS_20174392719_1491204439457_log.csv')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(dataset.shape)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
