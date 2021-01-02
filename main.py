93e55f9410de626d46d06fb102efc9a9d0d2cc0a
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-02 18:00:42 -0500

    using Logistic Regression for the training set

--- main.py
+++ main.py
@@ -26,5 +26,13 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 #feature scaling: all values are within similar range
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#now fit multiple linear regressions to the training set
+from sklearn.linear_model import LogisticRegression
+regressor = LogisticRegression()
 
 
