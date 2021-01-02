8375835abff7c2b56755340afc57929a636a2374
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-02 17:56:14 -0500

    more of using sklearn for creating the data set that will train the model

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ label_encoder = LabelEncoder()
 X[:, 0] = label_encoder.fit_transform(X[:, 0])
 
 #then we retransform into numbers b/c OneHotEncoder can't use strings
+one_hot_encoder = OneHotEncoder(categorial_features=[0])
+X = one_hot_encoder.fit_transform(X).toarray()
+
+#now wesplit the dataset into the Training set and the Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+#feature scaling: all values are within similar range
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
