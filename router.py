17f6597ad5040fb52afb0d14e67b1d4402939876
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-04 01:16:22 -0500

    fleshing out the api to fetch the returned value from our actual model

--- router.py
+++ router.py
@@ -1,7 +1,20 @@
 from flask import Flask, jsonify
 from flask_restful import Api
+import fraud_detection
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route("/api")
+def index():
+    return "this is a test! successfully launched py server"
+
+@app.route("/api/fraud-det")
+def fetch_model():
+    result = fraud_detection.train_model()
+    return (jsonify(result), 200)
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=9999)
 
 #currently following this tutorial: https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
