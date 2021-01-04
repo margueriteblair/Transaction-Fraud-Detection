4de3ee35c393607409c04ff1aa2a2c446f2d0b73
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-04 01:09:37 -0500

    using pip to install flask_restful

--- router.py
+++ router.py
@@ -2,3 +2,6 @@ from flask import Flask, jsonify
 from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
+
+#currently following this tutorial: https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
